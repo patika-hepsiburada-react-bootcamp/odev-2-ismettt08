80c6e43a753e3522fe96cf2751d314b23b6bab15
Ismet Taha Arslan <ismet.taha@gmail.com>
2021-10-02 23:55:29 +0300

    Random word api is set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+
+// https://random-word-api.herokuapp.com/word?number=1
+
 function App() {
-  return <div>Hello</div>;
+  const [word, setWord] = useState(null);
+  const [loadWord, setLoadWord] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    //API request here
+    if (loadWord === true) {
+      setIsLoading(true);
+      fetch("https://random-word-api.herokuapp.com/word?number=1")
+        .then((res) => res.json())
+        .then((data) => {
+          setWord(data);
+          setIsLoading(false);
+        })
+        .finally(() => {
+          setLoadWord(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [loadWord]);
+
+  return (
+    <div>
+      {isLoading ? "Please wait the word" : word}
+      <input
+        type="button"
+        onClick={() => {
+          setLoadWord(true);
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
